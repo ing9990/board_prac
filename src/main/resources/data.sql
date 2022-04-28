4774f9fd02be6ed47a789b999ed3239635d4c1cf
TaeWoo <43276268+ing9990@users.noreply.github.com>
2022-04-28 10:11:32 +0900

    TEST SQL (INSERT QUERY)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE BOARD_PRAC;
+
+USE BOARD_PRAC;
+SELECT * FROM BOARD;
+
+
+INSERT INTO BOARD VALUES(NULL,"잘 모르겠다",now(), 0 ,"1234","[CPU에 대해서 알아보자]",0,now(),"Taewoo");
+INSERT INTO BOARD VALUES(NULL,"램은 주 기억장치다",now(), 0 ,"1234","[ RAM이란 무엇일까?]",0,now(),"RAM MASTER");
+INSERT INTO BOARD VALUES(NULL,"하드 디스크는 보조 기억장치다.",now(), 0 ,"1234","[하드 디스크에 대해서]",0,now(),"하드디스크");
+INSERT INTO BOARD VALUES(NULL,"잘 알겠다.",now(), 0 ,"1234","[JPA란?]",0,now(),"YoungHwan");
+INSERT INTO BOARD VALUES(NULL,"Spring boot + JPA",now(), 0 ,"1234","[스프링부트에 대해서]",0,now(),"나영환");
+INSERT INTO BOARD VALUES(NULL,"dpi edpi 조절",now(), 0 ,"1234","[마우스 사용 방법]",0,now(),"태우김");
